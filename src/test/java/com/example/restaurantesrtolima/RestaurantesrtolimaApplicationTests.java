edd3d6efd93ab81fd4ea5b45d78ae86430b002cb
camilo.culm28@gmail.com <you@example.com>
2023-02-28 20:45:52 -0500

    se cre conexion a base de datos

---
+++ src/test/java/com/example/restaurantesrtolima/RestaurantesrtolimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantesrtolima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantesrtolimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
